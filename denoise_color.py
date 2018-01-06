d1d9a5c3341d7e559e46bbb8a1b7d74a15248ce8
Sergiu Breban <breban.sergiu94@gmail.com>
2018-01-06 12:21:00 +0200

    Add files

---
+++ denoise_color.py
@@ -0,0 +1,8 @@
+import cv2
+from matplotlib import pyplot as plt
+
+img = cv2.imread('die.png')
+dst = cv2.fastNlMeansDenoisingColored(img, None, 10, 10, 7, 21)
+plt.subplot(121), plt.imshow(img)
+plt.subplot(122), plt.imshow(dst)
+plt.show()
